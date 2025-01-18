7eb0878c0a04b56ed2cafc3d23de45015e525efa
sehanhasan <sehan@izecon.com>
2025-01-18 08:14:18 +0800

    update services grid

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -28,7 +28,7 @@ export const Services = () => {
             We are the expert in home, office & shop renovation and maintenance services providing you the one-stop solution to your needs.
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {services.map((service, index) => (
             <div 
               key={service.title}
